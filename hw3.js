8ac1a2e4dbfe188a8007f885f64eb5306408ce30
Ashley Radford <aradford7@outlook.com>
2019-03-22 00:21:51 -0700

    Commit 6 - Prime Numbers

--- hw3.js
+++ hw3.js
@@ -82,7 +82,71 @@ console.log(sumDigits(1148)); //answer 14  */
 //C^1/2 = A^2 + B^2 if 4 and 3
 // 16+9 = 25^1/2 = 5 goal is 5
 
-function calculateSide(sideA, sideB){
+/*function calculateSide(sideA, sideB){
     return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
 }
-console.log(calculateSide(4,3));
+console.log(calculateSide(4,3)); //Answer 5 YESYEYSYSYSYSYSYSY */
+
+//5.Prime Numbers
+//Step1: Write a function called checkPrime that test whether a number
+//is prime. The function will return true (Boolean) if Prime, false if not.
+
+//PseudoCode
+//Create array bc loop... assign value of true
+//Take square root of value
+//loop from two until new square root
+//is index prime if not false
+//loop (i*i)+x 
+//if not prime number = false
+
+/*function test_prime(n){
+    if (n===1){
+    return false;
+  }
+  else if(n === 2){
+    return true;
+  }else{
+    for(var x = 2; x < n; x++){
+      if(n % x === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+console.log(test_prime(50)); */
+
+//Step2: Write another function called printPrimes that will print
+//console.log all the pimes up in an limit so printPrime (97)
+//will print prime number up to 97 and including 97
+
+function test_prime(n){
+    if (n===1){
+    return false;
+  }
+  else if(n === 2){
+    return true;
+  }else{
+    for(var x = 2; x < n; x++){
+      if(n % x === 0) {
+        return false;
+    function printprimenumber(number){
+        if(checkprimenumber(number) == true){
+        console.log(number+" is Prime Number ");{
+        }
+    }
+    
+        function printnumbers(range){
+             for (i=1; i<=range; i++){
+                 printprimenumber(i);{
+                     }
+            }
+        }
+    }
+            var maxnumber = 97;
+                printnumbers(maxnumber);
+                }
+            }
+    }
+}
